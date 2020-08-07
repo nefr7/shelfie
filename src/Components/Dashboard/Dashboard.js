bdd9adfe88d66be9a0df3c3eb12e33d4ba72cd05
Michael Phillips <mcp191@icloud.com>
2020-08-07 10:53:56 -0500

    initial commit

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+// MyComponent.js
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>This is my component.</div>
+    );
+  }
+}
+
+export default MyComponent;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,12 @@
+// Product.js
+import React, { Product } from 'react';
+
+class Product extends Product {
+  render() {
+    return (
+      <div>This is my Product.</div>
+    );
+  }
+}
+
+export default Product;
